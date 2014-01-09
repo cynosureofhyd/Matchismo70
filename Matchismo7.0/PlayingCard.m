d8fa975fca632588937362367c6f198ef8aba1cd
Cynosure <Cynosure@Cynosures-MacBook-Pro.local>
2014-01-09 00:25:51 -0700

    PlayingCard

---
+++ Matchismo7.0/PlayingCard.m
@@ -0,0 +1,53 @@
+//
+//  PlayingCard.m
+//  Matchismo7.0
+//
+//  Created by Cynosure on 1/8/14.
+//  Copyright (c) 2014 Cynosure. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@implementation PlayingCard
+
+- (NSString *)contents
+{
+    NSArray *rankStrings = [PlayingCard rankStrings];
+    
+    return [rankStrings[self.rank] stringByAppendingString:self.suit];
+}
+
++(NSArray *)rankStrings
+{
+     NSArray *rankStrings = @[@"?", @"A", @"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"J", @"Q", @"K"];
+    return rankStrings;
+}
+
++(NSUInteger)maxRank
+{
+    return [self rankStrings].count - 1;
+}
+
+@synthesize suit = _suit;
+
++(NSArray *)validSuits
+{
+    static NSArray *validSuits = nil;
+    if(!validSuits)
+        validSuits = @[@"♥︎", @"♦︎", @"♠︎", @"♣︎"];
+    return validSuits;
+}
+
+-(NSString *)suit
+{
+    return _suit ? _suit : @"?";
+}
+
+
+-(void)setSuit:(NSString *)suit
+{
+    if([[PlayingCard validSuits] containsObject:suit])
+        _suit = suit;
+}
+
+@end
