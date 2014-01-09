e4defecc9bf94e31851bbaf454586e5cf95bea2e
Cynosure <Cynosure@Cynosures-MacBook-Pro.local>
2014-01-09 00:36:40 -0700

    PlayingCardDeck

---
+++ Matchismo7.0/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo7.0
+//
+//  Created by Cynosure on 1/9/14.
+//  Copyright (c) 2014 Cynosure. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
